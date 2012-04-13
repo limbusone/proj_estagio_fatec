1f353c86d79abfca7517571ce06a3bb1f228cf57
leo <leo@leo-ubuntu.(none)>
2012-04-13 17:02:43 -0300

    all ready

--- db/migrate/20120409021504_create_convenios.rb
+++ db/migrate/20120409021504_create_convenios.rb
@@ -1,6 +1,5 @@
 class CreateConvenios < ActiveRecord::Migration
 	def change
-		drop_table :convenios
 		create_table :convenios do |t|
 			t.integer :empresa_id, :null => false, :unique => true
 			t.integer :interveniente_id, :null => false
@@ -8,17 +7,6 @@ class CreateConvenios < ActiveRecord::Migration
 			t.boolean :rescidido, :default => false
 			t.timestamps
 		end
-		execute <<-COCO
-			ALTER TABLE convenios
-				ADD CONSTRAINT fk_convenios_empresas
-				FOREIGN KEY (empresa_id)
-				REFERENCES empresas(id)
-		COCO
-		execute <<-CACA
-			ALTER TABLE convenios
-				ADD CONSTRAINT fk_convenios_intervenientes
-				FOREIGN KEY (interveniente_id)
-				REFERENCES intervenientes(id)
-		CACA
+        add_index :convenios, :empresa_id
 	end
 end
