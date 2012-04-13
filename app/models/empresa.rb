29994d266cc187b1d7658da465c219d631677618
Dglgmut <florez.renan@gmail.com>
2012-04-13 18:21:25 -0300

    Single table inharitance for empresa

--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -1,4 +1,12 @@
 class Empresa < ActiveRecord::Base
-	attr_accessible :bairro, :cep, :cidade, :cnpj, :email, :estado, :logradouro, :nome, :numero, :telefone
-	has_one :convenio
+  attr_accessible :cnpj, :email, :nome, :telefone, :endereco, :endereco_id
+  has_one :endereco
+end
+
+class Contratante < Empresa
+  
+end
+
+class Intervenientes < Empresa
+  
 end
