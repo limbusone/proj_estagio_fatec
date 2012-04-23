4ee1213dbffd42373b0b31b114e6099c32dd0a70
Dglgmut <florez.renan@gmail.com>
2012-04-23 15:08:45 -0300

    Fix typo

--- app/models/convenio.rb
+++ app/models/convenio.rb
@@ -2,13 +2,11 @@
 class Convenio < ActiveRecord::Base
   attr_accessible :concedente_id, :interveniente_id, :rescidido, :vigencia
 
-	validates_presence_of :concedente_id, :message => "é necessário ser preenchido"
+  validates_presence_of :concedente_id, :message => "é necessário ser preenchido"
+  validates_presence_of :interveniente_id, :message => "é necessário ser preenchido"
 
-	validates_presence_of :interveniente_id, :message => "é necessário ser preenchido"
+  validates   :vigencia, :numericality => { :only_integer => true, :message => "somente caracteres numéricos" }, :allow_blank => true
 
-  validates   :vigencia, :numericality => { :only_integer => true, :message => 
-                "somente caracteres numéricos" }, :allow_blank => true
-  
   belongs_to :interveniente
-	belongs_to :concedente
+  belongs_to :concedente
 end
