72ba24dbd4d04f11c94e67b88d0c9f7f5d9e84d4
leo <leo@leo-ubuntu-pc.(none)>
2012-05-25 17:35:56 -0300

    jQuerying horario

--- app/assets/javascripts/scriptaux.js
+++ app/assets/javascripts/scriptaux.js
@@ -18,9 +18,19 @@ jQuery(function()
     newHorarioFimTex
     tField.setAttribute("name", "final[]");
     */
-
-      var l
-      var segunda_inicio_hora = event.target.parentNode.getElementById("segunda_inicio__hour").cloneNode(true);
-      });ab
+    var cDivFields = event.target.parentNode;
+    cDivFields
+    $(cDivFields).find("label:eq(0)").clone().appendTo(cDivFields);
+    $(cDivFields).find("select:eq(0)").clone().appendTo(cDivFields);
+    $(cDivFields).append(" : ");
+    $(cDivFields).find("select:eq(1)").clone().appendTo(cDivFields);
+    $(cDivFields).find("label:eq(1)").clone().appendTo(cDivFields);
+    $(cDivFields).find("select:eq(2)").clone().appendTo(cDivFields);
+    $(cDivFields).append(" : ");
+    $(cDivFields).find("select:eq(3)").clone().appendTo(cDivFields);
+    //$(cDivFields).find("select:last").clone().appendTo(cDivFields);    
+    //var label_select_inicio = event.target.parentNode.getElementsByTagName("label")[0].cloneNode(true);
+    //var select_inicio = event.target.parentNode.getElementsByTagName("select")[0].cloneNode(true);
+  });
 
 })
